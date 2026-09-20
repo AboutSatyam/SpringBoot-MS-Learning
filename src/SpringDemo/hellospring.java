447b44ed0aed62d951be4355836772fc47fbd9aa
AboutSatyam <programmersatyam07@gmail.com>
2026-09-20 15:19:37 +0530

    Update

--- src/SpringDemo/hellospring.java
+++ src/SpringDemo/hellospring.java
@@ -3,7 +3,7 @@ package SpringDemo;
 public class hellospring {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello  d  World");
+		System.out.println("Hello World");
 	}
 
 }
